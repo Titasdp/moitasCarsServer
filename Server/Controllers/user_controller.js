857ed0831490a0de332a624de95a4e3dda75d3ce
Tiago De Pina <tiagopina20014@gmail..com>
2021-03-10 18:17:46 +0000

    User controller updated

--- Server/Controllers/user_controller.js
+++ Server/Controllers/user_controller.js
@@ -4,7 +4,66 @@ const sequelize = require("../Database/connection")
 const encryptPack = require("../Middleware/encrypt")
 const uniqid = require('uniqid');
 
-getUser = (req, callback) => {
+userLogin = (data, callback) => {
+    encryptPack.decryptPassword({
+        password: data.user.password,
+        hash: data.hash
+    }, (isError, decryptResult) => {
+        if (isError) {
+            return callback(false, {
+                msg: "something went wrong please try agin, later",
+                error: decryptResult,
+                respCode: 500,
+                token: null
+            })
+        } else {
+            if (decryptResult) {
+                tokenMiddleware.generateToken({
+                        user: {
+                            id: data.user.id_user,
+                            userCode: "moitasCars"
+                        }
+                    },
+                    token => {
+                        return callback({
+                            msg: "Successful Login",
+                            error: null,
+                            token: token,
+                            respCode: 200,
+                            username: data.user.id_user
+                        });
+                    }
+                );
+            } else {
+                return callback(false, {
+                    msg: "The username and password you entered did not match our records.",
+                    error: "",
+                    respCode: 400,
+                    token: null
+                })
+            }
+        }
+    })
+}
+
+getUserByName = (req, callback) => {
+    sequelize
+        .query("SELECT * FROM user where user.name = :name user.filed!= 1", {
+            replacements: {
+                name: req.sanitize(req.body.username)
+            }
+        }, {
+            model: userModel.User
+        })
+        .then(data => {
+            return callback(true, data[0])
+        })
+        .catch(error => {
+            return callback(false, error)
+        });
+};
+
+getUsers = (req, callback) => {
     sequelize
         .query("SELECT * FROM user", {
             model: userModel.User
@@ -16,15 +75,16 @@ getUser = (req, callback) => {
             return callback(false, error)
         });
 };
+
 addUser = (req, callback) => {
-    const dataObj = {}
     if (req.body.password && req.body.name) {
         encryptPack.encryptPassword(req.sanitize(req.body.password), (isError, encryptResult) => {
             if (isError) {
-                dataObj.data = null
-                dataObj.msg = "Something went wrong please try again later"
-                dataObj.error = encryptResult
-                return callback(false, dataObj)
+                return callback(false, {
+                    data: null,
+                    msg: "Something went wrong please try again later",
+                    error: encryptResult
+                })
             } else {
                 sequelize
                     .query(
@@ -41,25 +101,27 @@ addUser = (req, callback) => {
                         }
                     )
                     .then(data => {
-                        dataObj.data = data
-                        dataObj.msg = "The user was created successfully"
-                        dataObj.error = error
-                        return callback(true, dataObj)
+                        return callback(true, {
+                            data: data,
+                            msg: "The user was created successfully",
+                            error: null
+                        })
                     })
                     .catch(insertError => {
-                        dataObj.data = null
-                        dataObj.msg = "Something went wrong please try again later"
-                        dataObj.error = insertError
-                        return callback(false, dataObj)
+                        return callback(false, {
+                            data: null,
+                            msg: "Something went wrong please try again later",
+                            error: insertError
+                        })
                     });
             }
-
         })
     } else {
-        dataObj.data = null
-        dataObj.msg = "There are fields that are empty, some fields must be filled"
-        dataObj.error = null
-        return callback(false, dataObj)
+        return callback(false, {
+            data: null,
+            msg: "There are fields that are empty, some fields must be filled",
+            error: null
+        })
     }
 };
 
@@ -103,10 +165,31 @@ fileUser = (req, callback) => {
         });
 }
 
+eliminateUser = (req, callback) => {
+    sequelize
+        .query(
+            "DELETE FROM user Where user.id_user = :id_user;", {
+                replacements: {
+                    id_user: req.sanitize(req.params.id_user),
+                }
+            }, {
+                model: userModel.User
+            }
+        )
+        .then(data => {
+            return callback(true, data)
+        })
+        .catch(error => {
+            return callback(true, error)
+        });
+}
 
 module.exports = {
     addUser,
     getUsers,
     updateUser,
-
+    fileUser,
+    eliminateUser,
+    getUserByName,
+    userLogin
 };
--- Server/Routes/routes.js
+++ Server/Routes/routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router(); // router
 const carController = require("../Controllers/car_controller")
 const brandController = require("../Controllers/brand_controller")
+const userController = require("../Controllers/user_controller")
+
 
 
 router.get("/cars", (req, res) => {
@@ -10,20 +12,8 @@ router.get("/cars", (req, res) => {
 
 //<Brand routes
 router.get("/brands", (req, res) => {
-    brandController.getBrands(req, (success, result) => {
-        if (success == true) {
-            res.status(200).json({
-                data: result,
-                message: "The request was an success",
-                error: null,
-            });
-        } else {
-            res.status(500).send({
-                data: null,
-                message: "Something went wrong please try again later",
-                error: result,
-            });
-        }
+    userController.getUserB(req, (success, result) => {
+
     });
 });
 // *Add Brand
@@ -122,6 +112,43 @@ router.patch("/brands", (req, res) => {
     });
 });
 //>
+
+
+//<User routes
+router.post("/login", (req, res) => {
+    userController.getUserByName(req, (success, result) => {
+        if (success == true) {
+            res.status(200).json("ctt");
+        } else {
+            res.status(400).send({
+                data: null,
+                message: "Something went wrong please try again later",
+                error: result,
+            });
+        }
+    });
+});
+
+
+router.post("/users", (req, res) => {
+    userController.addUser(req, (success, result) => {
+        if (success == true) {
+            res.status(200).json(result);
+        } else {
+            res.status(400).send({
+                data: null,
+                message: "Something went wrong please try again later",
+                error: result,
+            });
+        }
+    });
+});
+
+
+
+
+
+//user routes>
 
 
 
